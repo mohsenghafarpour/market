10e7baf3ad80a5b8c112f7d31fa3a389706f4ef9
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-09 02:14:37 +0430

    Use base activity

---
+++ app/src/main/java/com/hafthastad/market/di/ViewModelModule.kt
@@ -0,0 +1,9 @@
+package com.hafthastad.market.di
+
+import com.hafthastad.market.ui.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { MainViewModel() }
+}
